f53d883b07fafff8ad78a49af2259e5b41a392bd
DESKTOP-0OBN2F0\rnals <gumin.chung@dataknows.ai>
2021-11-24 15:13:04 +0900

    최초 커밋 / 최초로 인증 페이지로 이동 후, 쿠키에 세션 저장하도록 작업

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityExam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }        
+
+        [Authorize]
+        [HttpGet("Secret")]
+        public IActionResult Secret()
+        {
+            return Ok();
+        }
+        
+        [HttpGet("Authenticate")]
+        public async Task<IActionResult> Authenticate()
+        {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "Bob"),
+                new Claim("Grandma.Says", "Very nice boy."),
+            };
+
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob K Foo"),
+                new Claim("DrivingLicense", "A+")
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+
+            await HttpContext.SignInAsync(userPrincipal);
+
+            return Redirect("/api/Account/Secret");
+        }
+    }
+}
+
